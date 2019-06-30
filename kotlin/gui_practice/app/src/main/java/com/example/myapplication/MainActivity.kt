3e021f96f11fc6568ff07eec23a9af45406145c7
ryouka <coolblossom.0130@gmail.com>
2019-06-30 11:41:23 +0900

    確認用ダイアログの追加

--- kotlin/gui_practice/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ kotlin/gui_practice/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,17 +4,95 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_layout.*
+import java.util.stream.Collectors
 
 class MainActivity : AppCompatActivity() {
+
+
+    fun showDialog(title: String, msg: String) {
+        val dialog = NoticeDialogFragment()
+        dialog.title = title
+        dialog.msg = msg
+        dialog.show(supportFragmentManager, "MainActivity")
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // button event [Plain Text]
+        imageButton_editText
+            .setOnClickListener{
+                showDialog("Pain Text",
+                    editText_inputText.text.toString())
+            }
+
+        // button event [Password]
+        imageButton_password
+            .setOnClickListener{
+                showDialog("Password",
+                    editText_password.text.toString())
+            }
+
+        // button event [Date]
+        imageButton_date
+            .setOnClickListener{
+                showDialog("Date",
+                    editText_date.text.toString())
+            }
+
+        // button event [Radio Buttons]
+        imageButton_radio
+            .setOnClickListener{
+                showDialog("Radio Buttons",
+                    listOf(radioBtn_japanese, radioBtn_french, radioBtn_chinese)
+                        .map {
+                            String.format("%s : %s\n", it.text, it.isChecked)
+                        }
+                        .stream()
+                        .collect(Collectors.joining())
+                )
+            }
+
+        // button event [Seek Bar]
+        imageButton_seek
+            .setOnClickListener{
+                showDialog("Seek Bar",
+                    String.format("volume: %d", seekBar_volume.progress)
+                )
+            }
+
+        // button event [Toggle Button]
+        imageButton_toggle
+            .setOnClickListener{
+                showDialog("Toggle Button",
+                    String.format("state: %s", tglBtn_state.isChecked)
+                )
+            }
+
+        // button event [Switch]
+        imageButton_switch
+            .setOnClickListener{
+                showDialog("Switch",
+                    String.format("state: %s", switch_state.isChecked)
+                )
+            }
+
+        // button event [Multiline Text]
+        imageButton_multiline
+            .setOnClickListener{
+                showDialog("Multiline Text",
+                    editText_multiline.text.toString()
+                )
+            }
+
+        // button event [CHECK]
         pushBtn_check
             .setOnClickListener{
-                val dialog = NoticeDialogFragment()
-                dialog.show(supportFragmentManager, "NoticeDialogFragment")
+                showDialog("CHECK",
+                    "pushed [CHECK] Button!"
+                )
             }
 
     }
--- kotlin/gui_practice/app/src/main/java/com/example/myapplication/NoticeDialogFragment.kt
+++ kotlin/gui_practice/app/src/main/java/com/example/myapplication/NoticeDialogFragment.kt
@@ -7,10 +7,15 @@ import android.support.v4.app.DialogFragment
 
 class NoticeDialogFragment : DialogFragment() {
 
+    var title: String = ""
+    var msg: String = ""
+
+
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val builder = AlertDialog.Builder(activity)
-        return builder.setMessage("message")
-            .setTitle("title")
+        return builder
+            .setTitle(title)
+            .setMessage(msg)
             .setPositiveButton("ok", null)
             .create()
     }
