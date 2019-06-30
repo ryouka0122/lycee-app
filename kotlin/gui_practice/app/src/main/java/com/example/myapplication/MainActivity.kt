1db53ad0ff2a3db18b86fd594bde9de159386818
ryouka <coolblossom.0130@gmail.com>
2019-06-30 12:42:27 +0900

    ClickListenerの処理をメソッド切り出し

--- kotlin/gui_practice/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ kotlin/gui_practice/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,98 +2,98 @@ package com.example.myapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_layout.*
 import java.util.stream.Collectors
 
 class MainActivity : AppCompatActivity() {
 
-
-    fun showDialog(title: String, msg: String) {
+    private fun showDialog(title: String, msg: String) {
         val dialog = NoticeDialogFragment()
         dialog.title = title
         dialog.msg = msg
         dialog.show(supportFragmentManager, "MainActivity")
     }
 
+    private fun clickListener(model: View, title: String, msg: ()->String) {
+        model.setOnClickListener{
+            showDialog(title, msg.invoke())
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // button event [Plain Text]
-        imageButton_editText
-            .setOnClickListener{
-                showDialog("Pain Text",
-                    editText_inputText.text.toString())
-            }
+        clickListener(imageButton_editText,
+            "Plain Text",
+            editText_inputText.text::toString
+        )
 
         // button event [Password]
-        imageButton_password
-            .setOnClickListener{
-                showDialog("Password",
-                    editText_password.text.toString())
-            }
+        clickListener(imageButton_password,
+            "Password",
+            editText_password.text::toString
+        )
 
         // button event [Date]
-        imageButton_date
-            .setOnClickListener{
-                showDialog("Date",
-                    editText_date.text.toString())
-            }
+        clickListener(imageButton_date,
+            "Date",
+            editText_date.text::toString
+        )
 
         // button event [Radio Buttons]
-        imageButton_radio
-            .setOnClickListener{
-                showDialog("Radio Buttons",
-                    listOf(radioBtn_japanese, radioBtn_french, radioBtn_chinese)
-                        .map {
-                            String.format("%s : %s\n", it.text, it.isChecked)
-                        }
-                        .stream()
-                        .collect(Collectors.joining())
-                )
+        clickListener(imageButton_radio,
+            "Radio Buttons",
+            fun (): String {
+                return listOf(radioBtn_japanese, radioBtn_french, radioBtn_chinese)
+                    .map {
+                        String.format("%s : %s\n", it.text, it.isChecked)
+                    }
+                    .stream()
+                    .collect(Collectors.joining())
             }
+        )
 
         // button event [Seek Bar]
-        imageButton_seek
-            .setOnClickListener{
-                showDialog("Seek Bar",
-                    String.format("volume: %d", seekBar_volume.progress)
-                )
+        clickListener(imageButton_seek,
+            "Seek Bar",
+            fun(): String {
+                return String.format("volume: %d", seekBar_volume.progress)
             }
+        )
 
         // button event [Toggle Button]
-        imageButton_toggle
-            .setOnClickListener{
-                showDialog("Toggle Button",
-                    String.format("state: %s", tglBtn_state.isChecked)
-                )
+        clickListener(imageButton_toggle,
+            "Toggle Button",
+            fun(): String {
+                return String.format("state: %s", tglBtn_state.isChecked)
             }
+        )
 
         // button event [Switch]
-        imageButton_switch
-            .setOnClickListener{
-                showDialog("Switch",
-                    String.format("state: %s", switch_state.isChecked)
-                )
+        clickListener(imageButton_switch,
+            "Toggle Button",
+            fun(): String {
+                return String.format("state: %s", switch_state.isChecked)
             }
+        )
 
         // button event [Multiline Text]
-        imageButton_multiline
-            .setOnClickListener{
-                showDialog("Multiline Text",
-                    editText_multiline.text.toString()
-                )
-            }
+        clickListener(imageButton_multiline,
+            "Multiline Text",
+            editText_multiline.text::toString
+        )
 
         // button event [CHECK]
-        pushBtn_check
-            .setOnClickListener{
-                showDialog("CHECK",
-                    "pushed [CHECK] Button!"
-                )
+        clickListener(pushBtn_check,
+            "CHECK",
+            fun(): String {
+                return "pushed [CHECK] Button!"
             }
+        )
 
     }
 }
